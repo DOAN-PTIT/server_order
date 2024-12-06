35765bf54bab37ea3399dc6e7afa1e956ac9b8a3
trungtruong02 <hoangtrungtruongyt2020@gmail.com>
2024-12-06 23:52:01 +0700

    config

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ configViewEngine(app);
 initWebRoutes(app)
 
 // Middleware bảo mật và cấu hình CORS
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.listen(PORT, () => {
